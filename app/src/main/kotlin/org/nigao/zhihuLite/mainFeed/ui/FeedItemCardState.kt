a1090c05a5bf0a29a345b103bd4be89ee37bd6b9
nigao <ni.gao@qq.com>
2025-11-16 16:59:00 +0800

    Refactor

---
+++ app/src/main/kotlin/org/nigao/zhihuLite/mainFeed/ui/FeedItemCardState.kt
@@ -0,0 +1,27 @@
+package org.nigao.zhihuLite.mainFeed.ui
+
+import org.nigao.zhihuLite.model.FeedItem
+
+data class FeedItemCardState(
+    val question: String,
+    val authorAvatarUrl: String,
+    val authorName: String,
+    val excerpt: String,
+    val imageThumbnails: List<String>?,
+    val voteUpCount: Int,
+    val commentCount: Int,
+    val updatedTime: Long,
+)
+
+fun FeedItem.toFeedCardState(): FeedItemCardState {
+    return FeedItemCardState(
+        question = target?.question?.title.toString(),
+        authorAvatarUrl = target?.author?.avatarUrl.toString(),
+        authorName = target?.author?.name.toString(),
+        excerpt = target?.excerptNew.toString(),
+        imageThumbnails = target?.thumbnails,
+        voteUpCount = target?.voteupCount ?: 0,
+        commentCount = target?.commentCount ?: 0,
+        updatedTime = target?.updatedTime ?: 0
+    )
+}
